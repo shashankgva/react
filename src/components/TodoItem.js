ed53d1b0c1b442eaa91a1da7600dc3af72108106
Shashank <shashankgva@gmail.com>
2019-01-17 08:32:24 +0530

    Initial Commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class TodoItem extends Component {
+	getStyle = () => {
+
+		return {
+			background: '#f4f4f4',
+			padding: '10px',
+			borderBottom: '1px #ccc dotted'
+			textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+		}
+	}
+
+	render() {
+		return (
+			<div style={ this.getStyle() }>
+				<p>{this.props.todo.title}</p>
+			</div>
+		);
+	}
+}
+
+//PropTypes
+TodoItem.propTypes = {
+	todo:PropTypes.object.isRequired
+}
+
+let itemStyle = {
+	backgroundColor: '#f4f4f4'
+}
+export default TodoItem;
